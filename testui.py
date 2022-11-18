fd297f0e4667319ae4794e66833c7d6bdabaaf4b
cao_sheng <cao_sheng@kingdee.com>
2022-11-18 13:51:50 +0800

    test

---
+++ testui.py
@@ -0,0 +1,11 @@
+from PyQt6 import QtWidgets, QtGui
+import sys
+from ui.untitled import Ui_MainWindow
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
